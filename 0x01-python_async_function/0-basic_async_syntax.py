e1342c585ad3ddda8ad0089ae21c5cc9e26f081e
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2024-11-19 23:11:30 +0100

    Done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """A python coroutine file"""
-import asyncio
 import random
+import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """perform some calculations"""
     wait_second = random.uniform(0, max_delay)
     await asyncio.sleep(wait_second)
     return wait_second
-
-
